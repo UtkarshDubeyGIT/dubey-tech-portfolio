495ba39b2e1893cb84d54a8507bd50828a94573b
UtkarshDubeyGIT <utkarsh.dubey.ug23@nsut.ac.in>
2025-12-21 00:22:02 +0530

    Refactor code structure for improved readability and maintainability

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -325,19 +325,9 @@ const gallery: Gallery = {
       orientation: "horizontal",
     },
     {
-      src: "/images/gallery/vertical-4.jpg",
-      alt: "image",
-      orientation: "vertical",
-    },
-    {
       src: "/images/gallery/horizontal-3.jpg",
       alt: "image",
       orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/vertical-1.jpg",
-      alt: "image",
-      orientation: "vertical",
     },
     {
       src: "/images/gallery/vertical-2.jpg",
@@ -353,11 +343,6 @@ const gallery: Gallery = {
       src: "/images/gallery/horizontal-4.jpg",
       alt: "image",
       orientation: "horizontal",
-    },
-    {
-      src: "/images/gallery/vertical-3.jpg",
-      alt: "image",
-      orientation: "vertical",
     },
   ],
 };
